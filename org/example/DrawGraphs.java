b42032c89273e69f6da3cb67be961e857864a22f
Toderry <106995468+Toderry@users.noreply.github.com>
2022-11-01 16:33:52 +0500

    Add files via upload

---
+++ org/example/DrawGraphs.java
@@ -0,0 +1,116 @@
+package org.example;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.block.BlockBorder;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.chart.title.TextTitle;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+
+public class DrawGraphs extends JFrame {
+    private final String title;
+    private double[] arrayOfX;
+    private double[] arrayOfY;
+
+
+    public double[] getArrayOfX() {
+        return arrayOfX;
+    }
+
+    public double[] getArrayOfY() {
+        return arrayOfY;
+    }
+
+    public DrawGraphs(String title, double[] arrayOfX, double[] arrayOfY) {
+        this.title = title;
+        this.arrayOfX = arrayOfX;
+        this.arrayOfY = arrayOfY;
+        initUI();
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+
+    private void initUI() {
+
+        XYDataset dataset = createDataset();
+        JFreeChart chart = createChart(dataset);
+
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+        chartPanel.setBackground(Color.white);
+        add(chartPanel);
+
+        pack();
+        setTitle("Line chart");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private XYDataset createDataset() {
+
+        var series = new XYSeries("");
+        int i = 0;
+        for(;i < getArrayOfX().length;i++){
+            series.add(getArrayOfX()[i],getArrayOfY()[i]);
+        }
+
+        var dataset = new XYSeriesCollection();
+        dataset.addSeries(series);
+
+        return dataset;
+    }
+
+    private JFreeChart createChart(XYDataset dataset) {
+
+        JFreeChart chart = ChartFactory.createXYLineChart(
+                getTitle(),
+                "-lg(E)",
+                "N",
+                dataset,
+                PlotOrientation.VERTICAL,
+                true,
+                true,
+                false
+        );
+
+        XYPlot plot = chart.getXYPlot();
+
+        var renderer = new XYLineAndShapeRenderer();
+        renderer.setSeriesPaint(0, Color.RED);
+        renderer.setSeriesStroke(0, new BasicStroke(2.0f));
+
+        plot.setRenderer(renderer);
+        plot.setBackgroundPaint(Color.white);
+
+        plot.setRangeGridlinesVisible(true);
+        plot.setRangeGridlinePaint(Color.BLACK);
+
+        plot.setDomainGridlinesVisible(true);
+        plot.setDomainGridlinePaint(Color.BLACK);
+
+        chart.getLegend().setFrame(BlockBorder.NONE);
+
+        chart.setTitle(new TextTitle(getTitle(),
+                        new Font("Serif", java.awt.Font.BOLD, 18)
+                )
+        );
+
+        return chart;
+    }
+
+}
